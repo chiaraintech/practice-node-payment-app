3482e03595d65f1cc671dcc5df801825ab5f8c19
chiaraintech <chiaraintech@gmail.com>
2021-04-17 19:58:33 +0100

    Changed routing so that instead of rendering a store.html it now redirects us to a page still called store, but having an extension of ejs.

--- server.js
+++ server.js
@@ -5,11 +5,30 @@ const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
 const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 //To embed serverside code inside our front-end HTML pages.
 //My front end will be using EJS in order to render the views.
-app.set('view engine', 'ejs');
-//I need to tell the app where all the static front end facing files are.
-app.use(express.static('public'));
+
+
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.static('public'))
+
+
+
+//ROUTE
+app.get('/store', function(req, res) {
+    fs.readFile('items.json', function(error, data) {
+      if (error) {
+        res.status(500).end()
+      } else {
+        res.render('store.ejs', {
+          stripePublicKey: stripePublicKey,
+          items: JSON.parse(data)
+        })
+      }
+    })
+  })
 
 app.listen(4000);
